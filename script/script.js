a534cde56ef1d1b7ae8ab9d165142627d4e6fea1
Tilda Yngman <Matilda.yngman@hyperisland.se>
2021-11-02 09:26:26 +0100

    timer-bar
    I need to changes to the js because now it is actually behavning like it is "loading"

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
-//javascript
+const timerBar = document.getElementsByClassName('timer-bar')[0];
+
+setInterval(() => {
+    const computedStyle = getComputedStyle(timerBar);
+    const width = parseFloat(computedStyle.getPropertyValue
+        ('--width')) || 0
+        timerBar.style.setProperty('--width', width + .1)
+}, 5)
