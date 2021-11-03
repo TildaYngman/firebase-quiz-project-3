377919e76b8bb84bf8b9e08cef3ea7cf96651f59
Tilda Yngman <Matilda.yngman@hyperisland.se>
2021-11-03 11:22:47 +0100

    HTML cards that are printing the answwers

--- script/quizlayout.js
+++ script/quizlayout.js
@@ -6,23 +6,26 @@ async function loadedQuestionsFromApi() {
     const questions = await response.json();
   
     return questions;
-  }
+}
+
+  let questionsAnswers = [];
+  let questionCounter = 0;
   
-  document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", async () => {
     let questions = [];
-  
+
     try {
-      questions = await loadedQuestionsFromApi();
+        questions = await loadedQuestionsFromApi();
     } catch (e) {
-      console.log("Error!");
-      console.log(e);
+        console.log("Error!");
+        console.log(e);
     }
     console.log(questions);
-    var questionsAnswers = questions.results.map((question) => {
-      return formatQuestion(question);
+    questionsAnswers = questions.results.map((question) => {
+        return formatQuestion(question);
     });
     console.log(questionsAnswers);
-  });
+});
   
   function formatQuestion(loadedQuestion) {
     const formattedQuestion = {
@@ -44,3 +47,18 @@ async function loadedQuestionsFromApi() {
     //choice = [choice1, choice2...] in formattedQuestion object
     return formattedQuestion;
   }
+
+  
+
+  function createPreviewCard(card) {
+    var wrapper = document.getElementById("postsSummaries")
+        let i = questionCounter;
+        wrapper.innerHTML = `<p>${questionsAnswers[i].question}</p> 
+        <button>${questionsAnswers[i].choice1}</button> 
+        <button>${questionsAnswers[i].choice2}</button> 
+        <button>${questionsAnswers[i].choice3}</button> 
+        <button>${questionsAnswers[i].choice4}</button>`;
+        questionCounter++;
+  };
+console.log(questionsAnswers);
+
